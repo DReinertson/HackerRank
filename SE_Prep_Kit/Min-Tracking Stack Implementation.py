b5f66ec3ba7fc29e18f9ed390e0a89f091b2bde6
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2026-02-04 23:22:51 -0800

    Update Min-Tracking Stack Implementation.py
    working solution
    
    My previous method wasn't keeping track of the min values throughout
    
    So in scenarios where stack = [10, 5], when 5 was removed from the stack, the minimum would stay 5 instead of changing to 10

--- SE_Prep_Kit/Min-Tracking Stack Implementation.py
+++ SE_Prep_Kit/Min-Tracking Stack Implementation.py
@@ -1,41 +1,32 @@
 def processCouponStackOperations(operations):
     # Write your code here
     
-    # class Stack (list):
-        
-    #     def push(self, item):
-    #         self.append(item)
-        
-    #     def pop(self):
-    #         return self.pop()
-        
-    #     def top(self):
-    #         return self[-1]
-            
     stack = []
+    min_stack = []
     results = []
-    # min = None
     
     for operation in operations: 
         # print("operation: ", operation)
         
         if 'push' in operation:
             name, value = operation.split(' ')
-            stack.append(int(value))
-            # if min is None:
-            #     min = value
-            # elif value < min:
-            #     min = value
+            val = int(value)
+            stack.append(val)
+            if not min_stack or val <= min_stack[-1]:
+                min_stack.append(val)
+            else:
+                min_stack.append(min_stack[-1])
             # print('stack after push: ', stack)
             
         elif len(stack) > 0:
             # print("in len(stack > 0)")
             if 'pop' in operation:
                 stack.pop()
+                min_stack.pop()
             elif 'top' in operation:
                 results.append(stack[-1])
             elif 'getMin' in operation:
                 # print('appending min to results: ', min)
-                results.append(min(stack))
+                results.append(min_stack[-1])
     
     return results
