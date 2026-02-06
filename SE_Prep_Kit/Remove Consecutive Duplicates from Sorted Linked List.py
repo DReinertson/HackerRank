4725ea0f8b807b847d5832ad164e6d3452ebc026
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2026-02-05 22:53:57 -0800

    Update Remove Consecutive Duplicates from Sorted Linked List.py
    In place removal of duplicates
    
    The directions asked for a duplicated linked list, but wanted to be more efficient.

--- SE_Prep_Kit/Remove Consecutive Duplicates from Sorted Linked List.py
+++ SE_Prep_Kit/Remove Consecutive Duplicates from Sorted Linked List.py
@@ -1,33 +1,12 @@
 def deleteDuplicates(head):
     # Write your code here
-    class Node:
-        def __init__(self, data):
-            self.data = data
-            self.next = None
-    
-    class LinkedList:
-        def __init__(self):
-            self.head = None
     
     currentNode = head
-    duplicated_linked_list = LinkedList()
     
-    while currentNode:
-        if not currentNode.next:
-            if not duplicated_linked_list.head:
-                duplicated_linked_list.head = Node(currentNode.data)
-                break
-            else:
-                duplicate_node.next = Node(currentNode.data)
-                break
-        if currentNode.data != currentNode.next.data:
-            if not duplicated_linked_list.head:
-                duplicated_linked_list.head = Node(currentNode.data)
-                duplicate_node = duplicated_linked_list.head
-            else:
-                duplicate_node.next = Node(currentNode.data)
-                duplicate_node = duplicate_node.next
-                
-        currentNode = currentNode.next
+    while currentNode and currentNode.next:
+        if currentNode.data == currentNode.next.data:
+            currentNode.next = currentNode.next.next
+        else:
+            currentNode = currentNode.next
     
-    return duplicated_linked_list.head
+    return head
