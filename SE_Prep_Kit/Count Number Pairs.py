fdeaca9dc7a6849e011f8ee4091c95355aae3139
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2026-02-05 21:44:51 -0800

    Update Count Number Pairs.py
    Single while loop with two counters moving instead of nested for loops

--- SE_Prep_Kit/Count Number Pairs.py
+++ SE_Prep_Kit/Count Number Pairs.py
@@ -7,13 +7,21 @@ def countAffordablePairs(prices, budget):
     if len(prices) <= 1:
         return 0
     
-    else:
-        for index, price in enumerate(prices):
-            for j in range(index + 1, len(prices)):
-                # print("i, j: ", i, j)
-                # print("i")
-                if prices[j] + price <= budget:
-                    valid_pairs_count += 1
+    i = 0
+    j = 1
+    
+    while i < len(prices)-1:
+        
+        if prices[i] + prices[j] <= budget:
+            valid_pairs_count += 1
+        
+        j += 1
+        
+        # print ('i, j before: ', i, j)
+        if j == len(prices):
+            i += 1
+            j = i + 1
+        # print ('i, j after: ', i, j)
                     
                     
     return valid_pairs_count
